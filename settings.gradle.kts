4e881eca66623e270865f4f1f157dd24559bfefa
Ilya Sokolov <ilya.sokolov@payanyway.ru>
2024-03-15 14:07:08 +0300

    chore: Настройка

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,6 @@
-pluginManagement {
-    repositories {
-        mavenCentral()
-        gradlePluginPortal()
-    }
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
+
 
 rootProject.name = "conventional-commits"
